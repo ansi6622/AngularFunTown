fc4b37d726e7ac4329038306bd5623ea21f2e9a1
Anthony Simpson <ansi6622@colorado.edu>
2016-04-25 09:59:16 -0600

    emily building angular app up

--- fun.js
+++ fun.js
@@ -1,12 +1,6 @@
-// angular.module('HelloNameApp', [])
-//   .controller('HelloNameController', function($scope){
-//     $scope.RedRocket = function () {
-//     $scope.theTask = "Hello " + $scope.inpoot +  "\! ";
-//   };
-// });
-angular.module('HelloUserApp', [])
-      .controller('HelloUserController', function($scope) {
-          $scope.NameChange = function () {
-              $scope.greeting = "Hello " + $scope.name;
-          };
-      });
+angular.module('HelloNameApp', [])
+  .controller('HelloNameController', function($scope){
+    $scope.RedRocket = function () {
+    $scope.theTask = "Hello " + $scope.inpoot +  "\! ";
+  };
+});
